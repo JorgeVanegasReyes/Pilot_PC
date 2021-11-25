d283f09618685f23e95bb5846c014c63d73b9dbb
JorgeVanegasReyes <reyesjedu@gmail.com>
2021-11-25 12:53:39 -0500

    Se agrega css a Error 404

--- LoopBack/BackendLaptops/src/datasources/mongodb.datasource.ts
+++ LoopBack/BackendLaptops/src/datasources/mongodb.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'mongodb',
   connector: 'mongodb',
-  url: 'mongodb+srv://JorgeReyes:vanega975@clusterprogweb.um1er.mongodb.net/PilotPC?retryWrites=true&w=majority',
+  url: '',
   host: '',
   port: 0,
   user: '',
