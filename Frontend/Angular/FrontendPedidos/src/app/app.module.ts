c47dd76117a94c008572c2d12efe4e52969de0a0
JorgeVanegasReyes <reyesjedu@gmail.com>
2021-11-27 10:21:40 -0500

    se arreglan algunos estilos css a la pagina de inicio

--- Frontend/Angular/FrontendPedidos/src/app/app.module.ts
+++ Frontend/Angular/FrontendPedidos/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
 import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
 import { InicioComponent } from './plantilla/inicio/inicio.component';
-import { ErrorComponent } from './plantillas/error/error.component';
+import { ErrorComponent } from './plantilla/error/error.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +20,7 @@ import { ErrorComponent } from './plantillas/error/error.component';
     BrowserModule,
     AppRoutingModule
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA], //para que angular entienda que no todos los componentes son de angular sino que hay de webcomponents
   providers: [],
   bootstrap: [AppComponent]
 })
